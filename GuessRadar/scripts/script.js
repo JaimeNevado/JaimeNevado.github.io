58484c2dfc30e3c69a5669597a7348fdc6ee58da
Jaime Nevado Farfán <djamperiio@gmail.com>
2025-06-18 14:03:36 +0200

    Web entrenamiento

--- GuessRadar/scripts/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const playButton = document.getElementById("play-btn");
-    
-    playButton.addEventListener("click", function() {
-        window.open("juegoPrincipal.html", "_self");
-    });
-});
